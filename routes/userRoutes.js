00dadcc5397b6bdf8bb0639d5f844d02802a64e6
Soliu-Ahmad <102731648+Soliu-Ahmad@users.noreply.github.com>
2023-11-29 14:48:56 +0100

    working on follow and unfollow

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { signUpUser, loginUser, logoutUser} = require('../controllers/userController');
+const { followUnFollowUser,signUpUser, loginUser, logoutUser} = require('../controllers/userController');
+const protectRoute = require('../middleware/protectRoutes');
 
 const router = express.Router();
 
 router.post("/signup", signUpUser)
 router.post("/login", loginUser)
 router.post("/logout", logoutUser)
+router.post("/follow/:id", protectRoute, followUnFollowUser) // toggle state(follow/unfollow)
 
 module.exports = router;
