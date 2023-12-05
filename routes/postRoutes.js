89c0cde838b3bdeb4693aa3e0655e414f2018504
Soliu-Ahmad <102731648+Soliu-Ahmad@users.noreply.github.com>
2023-12-05 12:51:00 +0100

    working on like and unlike post

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,5 +1,5 @@
 const express = require ('express');
-const {createPost, getPost, deletePost} = require( "../controllers/postController")
+const {createPost, getPost, deletePost, likeUnlikePost} = require( "../controllers/postController")
 const protectRoute = require ('../middleware/protectRoutes');
 
 const router = express.Router();
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get("/:id", getPost)
 router.delete("/:id", protectRoute, deletePost);
 router.post("/create", protectRoute, createPost)
+router.post("/like/:id", protectRoute, likeUnlikePost);
 
 
 module.exports = router
